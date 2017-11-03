088513ae30c36aaada658f6ba0061db9e3432f69
Catislin <caitlin.estrellita@gmail.com>
2017-11-03 13:47:07 -0700

    added elements json object and function to combine elements

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ const express = require('express');
 const app = express();
 const hb = require('express-handlebars');
 
+const elementsArray = require('./elements.json');
+
+var createElement = (elements) => {
+  var totalProtons = 0;
+  for (let i = 0; i < elements.length; i++) {
+    totalProtons += elements[i].protons;
+  }
+  console.log('You just made: ' + getElementByProtonNumber(totalProtons).name)
+}
+
+var getElementByProtonNumber = (protonNumber) => {
+  for (let i = 0; i < elementsArray.length; i++) {
+    if (elementsArray[i].protons == protonNumber) {
+      return elementsArray[i];
+    }
+  }
+}
+
+var getElementByAbbrv = (elementAbbrv) => {
+  for (let i = 0; i < elementsArray.length; i++) {
+    if (elementsArray[i].abbrv == elementAbbrv) {
+      console.log(elementsArray[i])
+      return elementsArray[i];
+    }
+  }
+}
+
 app.get('/', function(req, res) {
-  res.send("Element Generator");
+  for (let i = 0; i < elementsArray.length; i++) {
+    console.log(elementsArray[i]);
+  }
+  var elementsToCombine = [getElementByAbbrv('H'), getElementByAbbrv('H')];
+  createElement(elementsToCombine);
+  res.send('Element Generator');
 })
 
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(req, res) {
-  console.log("listening!");
+  console.log('listening');
 });
