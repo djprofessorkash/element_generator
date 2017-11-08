6f4575d67d54ec82ca4711ec447d2efc8d3dbd3f
Unknown <caitlin.estrellita@gmail.com>
2017-11-07 23:04:18 -0800

    added user model

---
+++ user-model.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    createdAt       : { type: Date }
+  , updatedAt       : { type: Date }
+  , password        : { type: String, select: false }
+  , username        : { type: String, required: true }
+  , points          : { type: Number}
+});
+
+UserSchema.pre('save', function(next){
+  // SET createdAt AND updatedAt
+  var now = new Date();
+  if ( !this.points ) {
+    this.points = 0;
+  }
+  this.updatedAt = now;
+  if ( !this.createdAt ) {
+    this.createdAt = now;
+  }
+
+  // ENCRYPT PASSWORD
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
+UserSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
