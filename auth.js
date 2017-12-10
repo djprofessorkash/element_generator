735d3dc49c481cebf4ce72229b5ed3205ff10b9d
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-12-10 00:55:55 -0800

    Commented and cleaned up auth

--- auth.js
+++ auth.js
@@ -11,9 +11,9 @@
 // ================================================================================
 
 
-const bodyParser = require("body-parser");
-const User = require("./user-model");
-const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");              // Requires Body-Parser
+const User = require("./user-model");                   // Requires User Model
+const jwt = require("jsonwebtoken");                    // Requires JSON Web Token
 
 
 // ================================================================================
@@ -23,45 +23,89 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (app) => {
 
-  // ========= ROUTE TO SEND DATA VIA POST REQUEST TO LOGIN (CHECK USER) ==========
+  // ==============================================================================
+  // ============================= MODULAR FUNCTIONS ==============================
+  // ==============================================================================
+
+
+    // ================ FUNCTION TO SEQUENTIALLY SAVE EACH ELEMENT ================
+    function displayElementsInProfile(unlockedElements) {
+      let listOfUnlockedElements = [];
+  
+      for (let iterator = 0; iterator < unlockedElements.length; iterator++) {
+        console.log(unlockedElements[iterator].name);
+        // console.log(unlockedElements[iterator].abbrv);
+        listOfUnlockedElements.push(unlockedElements[iterator].name);
+      }
+  
+      console.log(listOfUnlockedElements);
+      return listOfUnlockedElements;
+    }
+
+
+  // ==============================================================================
+  // ============================== MODULAR ROUTES ================================
+  // ==============================================================================
+
+  // ========================= GET REQUEST TO LOGIN USER ==========================
+  app.get("/login", (req, res) => {
+    res.redirect("/");
+  });
+
+  // ====== POST REQUEST TO SEND DATA VIA POST REQUEST TO LOGIN (CHECK USER) ======
   app.post("/login", function(req, res, next) {
-    User.findOne({ username: req.body.username }, "+password", (err, user) => {
+
+    // Check if user exists from login data, then successfully login or redirect
+    User
+      .findOne({ username: req.body.username }, "+password", (err, user) => {
         if (!user) {
-          console.log('could not find user')
-          return res.render('home', {warningMessage: 'Wrong username or password'})
+          console.log("USER NOT FOUND.");
+          return res.render("home", {warningMessage: "Wrong username or password."});
         };
 
-        user.comparePassword(req.body.password, (err, isMatch) => {
-          if (!isMatch) {
-            console.log('password did not match')
-            return res.render('home', {warningMessage: 'Wrong username or password'})
-          }
+        // Verify password and JWT (SECRET) validity for successful login
+        user
+          .comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+              console.log("PASSWORD IS INCORRECT.");
+              return res.render("home", {warningMessage: "Wrong username or password."});
+            }
 
-          let token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: "60 days" });
+            let token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: "60 days" });
 
-          res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
-          res.redirect("/");
-        });
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.redirect("/");
+          });
       });
   });
 
-  // ============================= ROUTE TO LOG OUT USER ============================
-  app.get("/logout", (req, res) => {
-    res.clearCookie("nToken");
-    res.redirect("/");
-  });
-
-  // ============================= ROUTE TO LOG IN USER =============================
-  app.get("/login", (req, res) => {
-    res.redirect("/");
-  })
-
-  // ==============================  ==============================
+  // ======================== GET REQUEST TO SIGN UP USER =========================
   app.get("/sign-up", (req, res) => {
     res.redirect("/");
   })
 
+  // ======================== POST REQUEST TO SIGN UP USER ========================
+  app.post("/sign-up", (req, res, next) => {
+    // create User and JWT
+    let user = new User(req.body);
+
+    user.save((err) => {
+      if (err) {
+        return res.status(400).send({ err: err });
+      }
+      // generate a JWT for this user from the user's id and the secret key
+      let token = jwt.sign({ id: user.id}, process.env.SECRET, { expiresIn: "60 days"});
+      // set the jwt as a cookie so that it will be included in
+      // future request from this user's client
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true});
+      res.redirect('/');
+    })
+  });
+
+  // ==================== GET REQUEST TO ACCESS USER PROFILE ======================
   app.get("/profile", (req, res) => {
+
+    // Check if users exists, then display unlocked user elements in profile
     if (req.user) {
       User
         .findById(req.user.id)
@@ -83,38 +127,11 @@ module.exports = (app) => {
       console.log("USER NOT FOUND.");
       res.redirect("/");
     }
-  })
+  });
 
-  // FUNCTION TO SAVE EACH ELEMENT TO ARRAY IN ORDER
-  function displayElementsInProfile(unlockedElements) {
-    let listOfUnlockedElements = [];
-
-    for (let iterator = 0; iterator < unlockedElements.length; iterator++) {
-      console.log(unlockedElements[iterator].name);
-      // console.log(unlockedElements[iterator].abbrv);
-      listOfUnlockedElements.push(unlockedElements[iterator].name);
-    }
-
-    console.log(listOfUnlockedElements);
-    return listOfUnlockedElements;
-  }
-
-
-  // sign-up
-  app.post("/sign-up", (req, res, next) => {
-    // create User and JWT
-    let user = new User(req.body);
-
-    user.save((err) => {
-      if (err) {
-        return res.status(400).send({ err: err });
-      }
-      // generate a JWT for this user from the user's id and the secret key
-      let token = jwt.sign({ id: user.id}, process.env.SECRET, { expiresIn: "60 days"});
-      // set the jwt as a cookie so that it will be included in
-      // future request from this user's client
-      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true});
-      res.redirect('/');
-    })
+  // ======================== GET REQUEST TO LOG OUT USER =========================
+  app.get("/logout", (req, res) => {
+    res.clearCookie("nToken");
+    res.redirect("/");
   });
 }
